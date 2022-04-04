d9cb8b2c3d20c713b9a3a01b47a82f7f36019a34
meriem <meriem-chorfi.0022@hotmail.com>
2022-04-04 16:19:21 +0200

    modif final app pour runnin serve meriem

--- app.py
+++ app.py
@@ -1,2 +1,48 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
 from flask import Flask, abort
 from flask import request, make_response, render_template
+
+import data
+import cmds
+
+from api import SITE_API
+
+# ↓ DO NOT MODIFY THIS PART ↓ #############################################
+# init flask app instance                                                 #
+app = Flask(__name__)                                                     #
+# setup with the configuration provided by the user / environment         #
+app.config.from_object('config.Development')                              #
+data.init_app(app)                                                        #
+cmds.init_app(app)                                                        #
+app.register_blueprint(SITE_API, url_prefix='/api')                       #
+# ↑ DO NOT MODIFY THIS PART ↑ #############################################
+
+@app.route('/Acceuil')
+def Acceuil():
+    app.logger.debug('serving root URL /')
+    return render_template('accueil.html')
+
+@app.route('/Villes')
+def Villes():
+    app.logger.debug('serving root URL /')
+    return render_template('ville.html')
+
+@app.route('/Contact')
+def Contact():
+    app.logger.debug('serving root URL /')
+    return render_template('contact.html')
+
+@app.route('/VillesInd')
+def ville_indiv():
+    app.logger.debug('serving root URL /')
+    return render_template('ville_indiv.html')
+
+
+
+if __name__ == "__main__":
+
+    app.run()
+# VIM MODLINE
+# vim: ai ts=4 sw=4 sts=4 expandtab fileencoding=utf8
